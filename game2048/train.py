e00ee22890cdc5862c08fa47c9af204d6a5feeab
YANJUN-TONG <44483441+YANJUN-TONG@users.noreply.github.com>
2019-01-13 23:09:40 +0800

    Update train.py

--- game2048/train.py
+++ game2048/train.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/cluster/home/it_stu83/tr2048_test')
+sys.path.append('/cluster/home/it_stu83/2048-api')
 
 from game2048.game import Game
 from game2048.agents import MyAgent
